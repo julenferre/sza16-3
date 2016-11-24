99cfb9cf30659669727739d767c9ddf706a0dc59
Mielotxim <ekaneiza8@gmail.com>
2016-11-25 00:45:39 +0100

    uztet logina ondo dijoala

--- PHP/logIn.php
+++ PHP/logIn.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<script src="../JS/balioztatu.js"></script>
-</head>
-<body>
-	<p><b>AUTENTIFIKATU ZAITEZ:</b></p>
-	<form id="login" name="login" onSubmit='logIn.php' method="POST">
-			<br />
-			<b>Erabiltzailea: </b>
-			<input type="text" id="user" name="user" />
-			<br />
-			<b>Pasahitza :</b>
-			<input type="password" id="pass" name="pass" />
-			<br />
-			<input type="submit" id="submit" value="Sartu" />
-	</form>
-	<br/><br/>
-	<p> <a href='../layout.html'> -=HOME=-</a> </p>
-</body>
+	<head>
+	</head>
+	<body>
+		<p><b>AUTENTIFIKATU ZAITEZ:</b></p>
+		<form id="login" name="login" onSubmit='logIn.php' method="POST">
+				<br />
+				<b>Erabiltzailea: </b>
+				<input type="text" id="user" name="user" />
+				<br />
+				<b>Pasahitza :</b>
+				<input type="password" id="pass" name="pass" />
+				<br />
+				<input type="submit" id="submit" value="Sartu" />
+		</form>
+		<br/><br/>
+		<p> <a href='../layout.html'> -=HOME=-</a> </p>
+	</body>
 </html>
 
 
@@ -25,5 +24,20 @@
 		session_start();
 		if(isset($_POST['user'],$_POST['pass']) && strlen($_POST['user'])!=0 && strlen($_POST['pass'])!=0){
 			echo"<p><font size='40'>TO JAKEROOOO !</font></p>";
+			$aux1=false;
+			$xml=simplexml_load_file('../XML/erabiltzaileak.xml');
+			foreach ($xml->xpath('//erabiltzailea') as $erab)
+			{
+				if($erab->eposta==$_POST['user'] && $erab->pasahitza==$_POST['pass']){
+					$aux1=true;
+				}
+			}
+			if($aux1){
+				$_SESSION['user']=$_POST['user'];
+				header("Location:../HTML/home.html")
+			}
+			else{
+				echo"<p><font size='40'>ESTA TODO MAL !</font></p>";
+			}
 		}
 ?>
