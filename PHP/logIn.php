7aed11fbd28855b11e1a608b140927f656e2335c
Julen Ferrero <julenferre@gmail.com>
2016-12-05 12:54:24 +0100

    Login-a eginda. (zuzenketa)

--- PHP/logIn.php
+++ PHP/logIn.php
@@ -1,7 +1,4 @@
 <?PHP
-	//DDBBra konektatu		
-	include "connect.php";
-	
 	$user = $_POST['user'];
 	$pass = $_POST['pass'];
 	$encpas = sha1($pass);
@@ -12,7 +9,7 @@
 	foreach ($users->xpath('//erabiltzailea') as $erabiltzailea)
 	{
 		if(!$correctUser){
-			if($erabiltzailea->izena == $user && $erabiltzailea->pasahitza == $encpas){
+			if(strcmp($erabiltzailea->izena,$user) === 0 && strcmp($erabiltzailea->pasahitza,$encpas) === 0){
 				$correctUser = TRUE;
 				echo ("zuzena");
 			}
